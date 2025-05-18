76a96e2da9f0fe77f19eac4b8fd246e45a3209bb
BLANCAT-G <skatjrdl030822@gmail.com>
2025-05-19 00:36:02 +0900

    서버 통신 기초

--- ProjectM_Client/Classes/GameScene.cpp
+++ ProjectM_Client/Classes/GameScene.cpp
@@ -1,4 +1,9 @@
 #include "GameScene.h"
+#include "network/HttpRequest.h"
+#include "network/HttpResponse.h"
+#include "network/HttpClient.h"
+#include "rapidjson/document.h"
+#include <iostream>
 
 USING_NS_CC;
 
@@ -21,8 +26,52 @@ bool GameScene::init()
 
 	this->addChild(plabel);
 
-
+	GameScene::startMatch("abc");
+	
+	
 	return true;
 }
+
+void GameScene::startMatch(const std::string& userId) {
+	network::HttpRequest* request = new network::HttpRequest();
+	request->setUrl("http://localhost:8080/match");
+	request->setRequestType(network::HttpRequest::Type::POST);
+	std::string data = "{\"userId\":\"" + userId + "\"}";
+	request->setRequestData(data.c_str(), data.length());
+
+	request->setHeaders({ "Content-Type: application/json" });
+
+	request->setResponseCallback([this, userId](network::HttpClient* client, network::HttpResponse* response) {
+		if (!response || !response->isSucceed()) return;
+		std::string res(response->getResponseData()->begin(), response->getResponseData()->end());;
+		rapidjson::Document d;
+		
+		d.Parse(res.c_str());
+		if (d.HasMember("gameId")) {
+			_gameId = d["gameId"].GetString();
+		}
+	});
+
+	network::HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void GameScene::onOpen(network::WebSocket* ws) {
+	 ws->send("hi");
+}
+
+ void GameScene::onMessage(network::WebSocket* ws, const network::WebSocket::Data& data){
+	CCLOG("서버로부터 메시지: %s", data.bytes);
+}
+
+void GameScene::onClose(network::WebSocket* ws)  {
+	CCLOG("WebSocket 닫힘");
+	delete ws;
+}
+
+ void GameScene::onError(network::WebSocket* ws, const network::WebSocket::ErrorCode& error) {
+	CCLOG("WebSocket 에러 발생");
+}
+
 
 
--- ProjectM_Client/Classes/GameScene.h
+++ ProjectM_Client/Classes/GameScene.h
@@ -1,14 +1,31 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "network/WebSocket.h"
 
-class GameScene : public cocos2d::Scene
+USING_NS_CC;
+
+class GameScene : public Scene, public network::WebSocket::Delegate
 {
+private:
+	network::WebSocket* _ws=nullptr;
+	std::string _gameId="null";
 public:
-	static cocos2d::Scene* createScene();
+	static Scene* createScene();
 
 	virtual bool init();
 
 	CREATE_FUNC(GameScene);
+
+	virtual void onOpen(network::WebSocket* ws);
+
+    virtual void onMessage(network::WebSocket* ws, const network::WebSocket::Data& data);
+
+    virtual void onClose(network::WebSocket* ws);
+
+	virtual void onError(network::WebSocket* ws, const network::WebSocket::ErrorCode& error);
+
+	void startMatch(const std::string& userid);
+
 };
 
